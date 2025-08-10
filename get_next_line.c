7cd5074acf5804933c30335b0ff4062729dd45b7
aoneil <aoneil@student.42berlin.de>
2025-08-10 17:10:04 +0200

    get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoneil <aoneil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 17:06:41 by aoneil            #+#    #+#             */
+/*   Updated: 2025/08/10 16:58:04 by aoneil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(char *s);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+
+char	*ft_read_loop(int fd, char *reminder, char *buffer)
+{
+	char	*tmp;
+	ssize_t	bytes;
+
+	while (!ft_strchr(reminder, '\n'))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+			return (free(buffer), free(reminder), reminder = NULL, NULL);
+		if (bytes == 0)
+			break ;
+		buffer[bytes] = '\0';
+		tmp = reminder;
+		reminder = ft_strjoin(tmp, buffer);
+		free(tmp);
+		if (!reminder)
+			return (free(buffer), NULL);
+	}
+	return (reminder);
+}
+
+char	*ft_read_line(int fd, char *reminder)
+{
+	char		*buffer;
+
+	buffer = NULL;
+	if (!reminder)
+		reminder = ft_strdup("");
+	if (!reminder)
+		return (free(buffer), NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (free(reminder), NULL);
+	reminder = ft_read_loop(fd, reminder, buffer);
+	free (buffer);
+	if (reminder[0] == '\0')
+		return (free(reminder), reminder = NULL, NULL);
+	return (reminder);
+}
+
+char	*cut_reminder(char *s)
+{
+	char	*nl;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0' && s[i] != '\n')
+		i++;
+	if (s[i] == '\0' || s[i + 1] == '\0')
+		return (NULL);
+	nl = ft_strdup(&s[i + 1]);
+	s[i + 1] = '\0';
+	return (nl);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*reminder;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		if (reminder)
+			free (reminder);
+		reminder = NULL;
+		return (NULL);
+	}
+	line = ft_read_line(fd, reminder);
+	if (!line)
+		return (NULL);
+	reminder = cut_reminder(line);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoneil <aoneil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 17:06:53 by aoneil            #+#    #+#             */
+/*   Updated: 2025/08/07 03:27:02 by aoneil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+char	*ft_substr(char const *s, size_t start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(char *src)
+{
+	size_t	len;
+	char	*dest;
+
+	len = 0;
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!dest)
+		return (NULL);
+	while (src[len])
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
