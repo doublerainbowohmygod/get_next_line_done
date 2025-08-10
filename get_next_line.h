315fb7b084b89bd217c6dfaf9ad6d7d9f5085bdf
aoneil <aoneil@student.42berlin.de>
2025-08-10 17:23:50 +0200

    gnl_l

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aoneil <aoneil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:07:11 by aoneil            #+#    #+#             */
-/*   Updated: 2025/08/07 17:40:45 by aoneil           ###   ########.fr       */
+/*   Updated: 2025/08/10 17:23:18 by aoneil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stddef.h>
-# include <stdint.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000000
 
 # endif
 
-// size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-// size_t	ft_strlcat(char *dst, const char *src, size_t size);
-// void	ft_bzero(void *s, size_t n);
-// void	*ft_calloc(size_t nmemb, size_t size);
-
 size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, size_t start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-
 char	*ft_strdup(char *s);
 char	*ft_strchr(const char *s, int c);
 char	*get_next_line(int fd);
 char	*cut_reminder(char	*line);
 char	*ft_read_line(int fd, char *reminder);
+char	*ft_read_loop(int fd, char *reminder, char *buffer);
 
 #endif
